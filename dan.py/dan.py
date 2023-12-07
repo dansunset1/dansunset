9398fc234c48527d051e6cd883ac0bcf77e71b2b
dansunset1 <153143860+dansunset1@users.noreply.github.com>
2023-12-07 08:30:47 +0800

    Add files via upload

---
+++ dan.py/dan.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Danilo's Weblog", page_icon=":tada:", layout="wide")
+
+with st.container():
+    st.title("Danilo's Weblog.")
+    st.write("Hi welcome to my blog! My name is Danilo C. Becerro Jr., 19 y/old. I came from the municipality of tubod. My hobbies is playing online games,volleyball,and practicing motorcycle. As a Computer Engineering student, our days are a harmonious blend of logic gates and creative coding, where lectures transform theoretical concepts into tangible solutions. Sleepless nights are not just a consequence but a testament to our dedication in deciphering the language of machines and crafting solutions to real-world challenges.")
+    st.image("dan.jpg", caption="Embrace your belief that there is always a room to grow, and allow yourself to evolve beyond your limitations.")
+    st.title("Content")
+    st.write("At the heart of computer engineering lies the synergy between theory and practice. Join us in our hands-on experiences, where we translate theoretical concepts into tangible solutions. Our projects, experiments, and real-world applications bridge the gap between classroom learning and the dynamic challenges of the tech industry.")
